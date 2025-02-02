0f01cd2e5021c20a3bceba6752ff8e7ad75e7fe6
lakshyaCreates <lakshya.creates07@gmail.com>
2025-02-02 11:39:51 +0530

    clear project & create new next app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,16 @@
-import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
-    {
-        rules: {
-            "no-unused-vars": "on",
-        },
-    },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
--- src/app/(routes)/_components/header.tsx
+++
@@ -1,9 +0,0 @@
-import { Logo } from "@/components/logo";
-
-export const Header = () => {
-    return (
-        <div className="absolute left-8 top-2 size-full sm:left-16 md:left-28 lg:left-36">
-            <Logo />
-        </div>
-    );
-};
