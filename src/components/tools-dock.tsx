14de66ede4b88c9043e774504e565c5d78ec5b7b
lakshyaCreates <lakshya.creates07@gmail.com>
2025-02-02 12:12:41 +0530

    feat: enable theme switching from the dock

--- src/components/tools-dock.tsx
+++ src/components/tools-dock.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import { CaseSensitive, Palette, Type } from "lucide-react";
+
+import { useTheme } from "next-themes";
 
 import { Dock, DockIcon, DockItem, DockLabel } from "./ui/dock";
 
 export const ToolsDock = () => {
+    const { theme, setTheme } = useTheme();
+
     return (
         <div className="absolute bottom-2 left-1/2 max-w-full -translate-x-1/2">
             <Dock className="items-end pb-3">
@@ -15,12 +21,18 @@ export const ToolsDock = () => {
                         <DockIcon>{item.icon}</DockIcon>
                     </DockItem>
                 ))}
-                <DockItem className="aspect-square rounded-full bg-gray-200 dark:bg-neutral-800">
-                    <DockLabel>Switch Theme</DockLabel>
-                    <DockIcon>
-                        <Palette className="size-full text-neutral-600 dark:text-neutral-300" />
-                    </DockIcon>
-                </DockItem>
+                <button
+                    onClick={() => {
+                        setTheme(theme === "light" ? "dark" : "light");
+                    }}
+                >
+                    <DockItem className="aspect-square rounded-full bg-gray-200 dark:bg-neutral-800">
+                        <DockLabel>Switch Theme</DockLabel>
+                        <DockIcon>
+                            <Palette className="size-full text-neutral-600 dark:text-neutral-300" />
+                        </DockIcon>
+                    </DockItem>
+                </button>
             </Dock>
         </div>
     );
