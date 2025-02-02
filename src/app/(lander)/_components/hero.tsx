6d0c2eb3ee8f4cb525bad16b209525cb75de5570
lakshyaCreates <lakshya.creates07@gmail.com>
2025-02-02 15:29:03 +0530

    feat(/): build hero

--- src/app/(lander)/_components/hero.tsx
+++ src/app/(lander)/_components/hero.tsx
@@ -1,3 +1,25 @@
+import Balancer from "react-wrap-balancer";
+
+import { Badge } from "@/components/ui/badge";
+
 export const Hero = () => {
-    return <div></div>;
+    return (
+        <div className="flex h-full min-w-full max-w-3xl flex-col items-center justify-center pt-24 text-center md:pt-36 lg:pt-48 xl:pt-60">
+            <Badge className="rounded-full border-none !bg-gradient-to-br from-primary via-muted/35 via-60% to-primary py-1">
+                We're live on Product Hunt
+            </Badge>
+            <Balancer>
+                <h1
+                    className="mt-6 text-4xl font-bold leading-[1.1] tracking-tight sm:text-5xl md:text-7xl"
+                    suppressHydrationWarning
+                >
+                    Find Your Perfect Font Pair
+                </h1>
+                <p className="mt-6 text-[17px] md:text-lg">
+                    Test and preview fonts in real-time for all your design
+                    needs. Choose the perfect typeface with ease.
+                </p>
+            </Balancer>
+        </div>
+    );
 };
