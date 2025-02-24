4ccf491823be0241c925c030a946e58d114eb0d2
lakshyaCreates <lakshya.creates07@gmail.com>
2025-02-24 18:26:31 +0530

    refactor(ui): decrease padding top of hero

--- src/app/(lander)/_components/hero.tsx
+++ src/app/(lander)/_components/hero.tsx
@@ -4,7 +4,7 @@ import { Badge } from "@/components/ui/badge";
 
 export const Hero = () => {
     return (
-        <div className="flex h-full min-w-full max-w-3xl flex-col items-center justify-center pt-24 text-center md:pt-36 lg:pt-48 xl:pt-60">
+        <div className="flex h-full min-w-full max-w-3xl flex-col items-center justify-center  text-center pt-36 lg:pt-44 xl:pt-52">
             <Badge className="rounded-full border-none !bg-gradient-to-br from-primary via-muted/35 via-60% to-primary py-1">
                 We're live on Product Hunt
             </Badge>
